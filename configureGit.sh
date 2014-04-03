88bca956cbeecdd6c60d9c43653764ab8237ff10
Masha Kourjanskaia <mkourjan@calpoly.edu>
2014-04-03 13:05:56 -0700

    added a documentation

--- configureGit.sh
+++ configureGit.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+# This script configures git whenever you start working
+# on multiple machines.  Run it any time you decide to work
+# with git on a new computer.
+
 git config --global user.name "Masha Kourjanskaia"
 git config --global user.email "mkourjan@calpoly.edu"
 git config --global color.ui "auto"
