b3b2686c65badcab31c53c4a3c0168a5fcecaccf
Masha Kourjanskaia <mkourjan@calpoly.edu>
2014-04-03 12:55:01 -0700

    adding config and pledge

---
+++ configureGit.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git config --global user.name "Masha Kourjanskaia"
+git config --global user.email "mkourjan@calpoly.edu"
+git config --global color.ui "auto"
+git config --global core.editor "nano"
